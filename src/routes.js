dcd73382b780bba6dad190223247ff332772f808
RandiRamadhani10 <80814841+RandiRamadhani10@users.noreply.github.com>
2022-05-29 21:57:45 +0700

    add service

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addbookhandler,
+  getAllBooksHandler,
+  getBooksByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addbookhandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBooksByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+];
+
+module.exports = routes;
